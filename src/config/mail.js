73139db5f086fb68f7cfbc2afd11f6dd29bb194e
ayronbanze0-blip <ayronbanze0@gmail.com>
2026-06-23 21:15:47 +0200

    mail.js

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,1 +1,13 @@
+'use strict';
 
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+module.exports = transporter;
