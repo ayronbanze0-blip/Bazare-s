552158c11d0694da30debebb716d80d6cba8634b
ayronbanze0-blip <ayronbanze0@gmail.com>
2026-06-24 05:45:32 +0200

    Update mail.js

--- src/config/mail.js
+++ src/config/mail.js
@@ -3,10 +3,12 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
   }
 });
 
