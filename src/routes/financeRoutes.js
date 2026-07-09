e5030282e682aeba47c676f3a54e439c34f23282
ayronbanze0-blip <ayronbanze0@gmail.com>
2026-07-09 20:54:54 +0200

    Update financeRoutes.js

--- src/routes/financeRoutes.js
+++ src/routes/financeRoutes.js
@@ -5,6 +5,7 @@ const ctrl = require('../controllers/financeController');
 const { authenticate, isSeller, isAdmin } = require('../middleware/auth');
 
 router.get('/me', authenticate, isSeller, ctrl.myFinance);
+router.get('/payments', authenticate, isSeller, ctrl.listPayments);
 router.post('/me/submit-payment', authenticate, isSeller, ctrl.submitPayment);
 router.post('/admin/:bazarId/confirm-payment', authenticate, isAdmin, ctrl.confirmPayment);
 router.patch('/admin/:bazarId/adjust-fee', authenticate, isAdmin, ctrl.adjustFee);
