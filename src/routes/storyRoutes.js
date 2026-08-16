2485a96dd040c55f852a67262c43bede261de9e5
ayronbanze0-blip <ayronbanze0@gmail.com>
2026-08-16 21:20:15 +0200

    Add files via upload

--- src/routes/storyRoutes.js
+++ src/routes/storyRoutes.js
@@ -3,12 +3,14 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/storyController');
 const { authenticate, optionalAuth } = require('../middleware/auth');
+const { uploadMedia } = require('../services/uploadService');
 
 router.get('/', optionalAuth, ctrl.list);
 router.post('/:id/view', authenticate, ctrl.markViewed);
 router.post('/:storyId/reply', authenticate, ctrl.reply);
 router.get('/:storyId/viewers', authenticate, ctrl.viewers);
-router.put('/:storyId', authenticate, ctrl.updateText);
+router.get('/:storyId', authenticate, ctrl.getOne);
+router.put('/:storyId', authenticate, uploadMedia.fields([{ name: 'image', maxCount: 1 }]), ctrl.updateText);
 router.delete('/:storyId', authenticate, ctrl.remove);
 
 module.exports = router;
