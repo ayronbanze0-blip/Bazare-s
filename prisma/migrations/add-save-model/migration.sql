666e43ec93a393cd372344bd5a96ed0def9eccfa
ayronbanze0-blip <ayronbanze0@gmail.com>
2026-09-26 02:57:05 +0200

    Add files via upload

--- prisma/migrations/add-save-model/migration.sql
+++ prisma/migrations/add-save-model/migration.sql
@@ -1,1 +1,22 @@
+-- CreateTable
+CREATE TABLE "Save" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "targetType" "FeedTargetType" NOT NULL,
+    "targetId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
+    CONSTRAINT "Save_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Save_targetType_targetId_idx" ON "Save"("targetType", "targetId");
+
+-- CreateIndex
+CREATE INDEX "Save_userId_idx" ON "Save"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Save_userId_targetType_targetId_key" ON "Save"("userId", "targetType", "targetId");
+
+-- AddForeignKey
+ALTER TABLE "Save" ADD CONSTRAINT "Save_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
